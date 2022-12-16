3537eb5b163d935721258afa74ee42ec7f716506
Aleksandr <sasha.saveylov@mail.ru>
2022-12-17 01:15:44 +0300

    14 pr completed

--- http-client/src/index.php
+++ http-client/src/index.php
@@ -1,75 +1,51 @@
 <html>
 <head>
-    <title>HTML Service</title>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Jost">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-    <style>
-        body {
-            font-family: "Jost", sans-serif;
-            color: white;
-            background-image: url('https://wallpaper.dog/large/445126.jpg');
-        }
-        .container {
-            border: 4px solid white;
-            border-radius: 15px;
-            background-color: rgb(26, 26, 26);
-            margin-top: 15px;
-            margin-left: auto;
-            margin-right: auto;
-        }
-        .list-group .list-group-item {
-            margin-top: 5px;
-            border: 1px solid black;
-            border-radius: 15px;
-            color: black;
-        }
-        .form-control.form-control-lg {
-            border: 2px solid black;
-            border-radius: 15px;
-        }
-        .btn.btn-info {
-            border: 2px solid white;
-            border-radius: 15px;
-        }
-    </style>
+ <title>Best forecasting ever</title>
 </head>
 <body>
-    <div class="container form">
-        <div class="container-fluid">
-            <h2>Simple Web Application</h2>
-        </div>
-        <h2>Find out your text fragments</h2>
-        <form action="index.php" method="post">
-            <div class="form-group">
-                <input id="url" name="url" class="form-control form-control-lg" placeholder="Type URL...">
-            </div>
-            <button type="submit" class="btn btn-info">Submit</button>
-        </form>
-    </div>
-    <br><br>
-    <?php
-    if (isset($_POST['url'])) {
+ <h2>Forecasting</h2>
+ <h3>First date, last date and color:</h3>
+ <?php
+    if (isset($_POST['first']) && isset($_POST['second']) && isset($_POST['third'])){
         $myCurl = curl_init();
         curl_setopt_array($myCurl, array(
-        CURLOPT_URL => 'http://nginxserver/api/?url=' . $_POST['url'],
-        CURLOPT_RETURNTRANSFER => true,
-        CURLOPT_HEADER => false,
-        )
-        );
+              CURLOPT_URL => 'http://nginxserver/api/?a='.$_POST['first'].'&b='.$_POST['second'].'&c='.$_POST['third'],
+                          CURLOPT_RETURNTRANSFER => true,
+                          CURLOPT_HEADER => false,
+        ));
         $response = curl_exec($myCurl);
         curl_close($myCurl);
-        $json = json_decode($response);
-        $url = $json->url;
-        $array = $json->text;
-        echo "<div class=\"container response\"><div class=\"container-fluid\"><h2>Найденные фрагменты</h2></div>";
-        echo "<ul class=\"list-group\">";
-        foreach($array as $value) {
-            echo "<li class=\"list-group-item\">" . $value . "</li>";
-        }
-        echo "</ul></div>";
+        $response_array = json_decode($response, true);
+        // var_dump($response_array);
+        $base64_code = $response_array['byte_code'];
+        $encoding = $response_array['encoding'];
+        // echo $base64_code;
+        $image_path = "./your_forecast.jpg";//путь к картинке относительный
+        $image_path_full= "/var/www/html/your_forecast.jpg"; // абсолютный путь к картинке
+        $fp = fopen($image_path_full, "w+");// открытие файла на запись[30]
+        fwrite($fp, base64_decode($base64_code)); //запись в файл декодированных байтов[31]
+        fclose($fp);// закрытие файла
+
+        // echo "Ответ на Ваш запрос: ".$response;
     }
-    ?>
+ ?>
+ <form action="index.php" method="post">
+ <label for="first_date">First date </label>
+ <input type="text" name="first" id="first" required>
+ <label for="last_date">Last date </label>
+ <input type="text" name="second" id="second" required>
+ <label for="color">Color </label>
+ <input type="text" name="third" id="third" required>
+ <input type="submit" value="GET GRAPHIC!">
+ </form>
+ <h3>Here appears your graphic or error</h3>
+ <?php
+    $lolkek = "Error";
+    if (strpos($response, $lolkek) !== false) {
+        echo $response;
+    } else {
+        echo '<img src="'.$image_path.'" width="700" height="500">';
+    }
+ ?>
 </body>
 </html>
